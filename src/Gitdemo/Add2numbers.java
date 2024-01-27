bfe64816fe50b8f7b7fb5a73f57da264b292761d
sumasree <saisumasreeatmuri@gmail.com>
2024-01-27 20:41:42 +0530

    Addition of 2 numbers

---
+++ src/Gitdemo/Add2numbers.java
@@ -0,0 +1,10 @@
+package Gitdemo;
+
+public class Add2numbers {
+	public static void main(String[] args) {
+	int num1=9;
+	int num2=8;
+	int sum = num1+num2;
+	System.out.println("Sum of the numbers" + sum);
+}
+}
