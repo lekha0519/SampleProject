f3cc4ddf9924f3977760932715b552e67e31bedc
sumasree <saisumasreeatmuri@gmail.com>
2024-01-27 20:17:34 +0530

    Initial commit test

---
+++ src/Gitdemo/DemoGit.java
@@ -0,0 +1,11 @@
+package Gitdemo;
+
+public class DemoGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Git Session");
+
+	}
+
+}
